194c18b8751a9d3c98c1d8d6c5a27d6c8f59624f
zumaad <khanzumaad@gmail.com>
2020-04-12 16:33:22 -0400

    Added functionality for freezing and unfreezing items

--- src/main/java/com/flippingutilities/FlippingItem.java
+++ src/main/java/com/flippingutilities/FlippingItem.java
@@ -143,21 +143,25 @@ public class FlippingItem
 	}
 
 	/**
-	 * This Method is responsible for freezing an item. When an item is to be frozen, buyPriceNeedsUpdate and
+	 * This Method is responsible for freezing an item's margin. When an item is to be frozen, buyPriceNeedsUpdate and
 	 * sellPriceNeeds update are set to true, and isFrozen is set to false. isFrozen is set to false so that
 	 * updateMargin will update the margins and so that the components that rely on a FlippingItem can easily
 	 * see that it is frozen. BuyPriceNeedsUpdate and sellPriceNeedsUpdate are set to true, so that in
 	 * {@link FlippingPlugin#updateFlippingItem(FlippingItem, OfferInfo)} when an item is being updated, the margin
 	 * is only frozen again if BOTH the sell price and buy price are updated.
-	 * @param freeze
+	 *
+	 * @param freeze whether the item should have it's margin frozen or not
 	 */
-	public void freezeItem(boolean freeze) {
-		if (freeze) {
+	public void freezeMargin(boolean freeze)
+	{
+		if (freeze)
+		{
 			isFrozen = true;
 			buyPriceNeedsUpdate = false;
 			sellPriceNeedsUpdate = false;
 		}
-		else {
+		else
+		{
 			isFrozen = false;
 			buyPriceNeedsUpdate = true;
 			sellPriceNeedsUpdate = true;
