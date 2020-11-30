923a20c82c3d7538112c6876b7a0f7df9039a29a
zumaad <khanzumaad@gmail.com>
2020-11-30 11:33:43 -0600

    color coding

--- src/main/java/com/flippingutilities/ui/flipping/FlippingItemPanel.java
+++ src/main/java/com/flippingutilities/ui/flipping/FlippingItemPanel.java
@@ -37,6 +37,7 @@ import static com.flippingutilities.ui.utilities.UIUtilities.STAR_OFF_ICON;
 import static com.flippingutilities.ui.utilities.UIUtilities.STAR_ON_ICON;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
@@ -57,6 +58,7 @@ import net.runelite.client.ui.DynamicGridLayout;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.util.AsyncBufferedImage;
 import net.runelite.client.util.QuantityFormatter;
+import org.pushingpixels.substance.api.UiThreadingViolationException;
 
 /**
  * Represents an instance of one of the many panels on the FlippingPanel. It is used to display information such as
@@ -91,8 +93,8 @@ public class FlippingItemPanel extends JPanel
 	JLabel latestBoughtAt = new JLabel();
 	JLabel latestSoldAt = new JLabel();
 
-	JLabel priceCheckBuyText = new JLabel("Should buy at: ");
-	JLabel priceCheckSellText = new JLabel("Should sell at: ");
+	JLabel priceCheckBuyText = new JLabel("Last margin buy: ");
+	JLabel priceCheckSellText = new JLabel("Last margin sell: ");
 	JLabel latestBuyPriceText = new JLabel("Last buy price: ");
 	JLabel latestSellPriceText = new JLabel("Last sell price: ");
 	JLabel profitEachText = new JLabel("Profit each: ");
@@ -162,8 +164,8 @@ public class FlippingItemPanel extends JPanel
 		JPanel potentialProfitPanel = new JPanel(new BorderLayout());
 
 		JPanel[] panels = {priceCheckBuyPanel, priceCheckSellPanel, latestBuyPanel, latestSellPanel, profitEachPanel, potentialProfitPanel};
-		JLabel[] descriptionLabels = {priceCheckBuyText, priceCheckSellText, latestBuyPriceText, latestSellPriceText, profitEachText, profitTotalText};
-		JLabel[] valueLabels = {priceCheckBuyVal, priceCheckSellVal, latestBuyPriceVal, latestSellPriceVal,profitEachVal, potentialProfitVal};
+		JLabel[] descriptionLabels = {latestBuyPriceText, latestSellPriceText, priceCheckBuyText, priceCheckSellText, profitEachText, profitTotalText};
+		JLabel[] valueLabels = {latestBuyPriceVal, latestSellPriceVal, priceCheckBuyVal, priceCheckSellVal,profitEachVal, potentialProfitVal};
 
 
 		for (int i=0;i<panels.length;i++) {
@@ -302,7 +304,7 @@ public class FlippingItemPanel extends JPanel
 
 	private void setValueLabels() {
 
-		Arrays.asList(priceCheckBuyVal, priceCheckSellVal, latestBuyPriceVal, latestSellPriceVal, profitEachVal, potentialProfitVal,
+		Arrays.asList(latestBuyPriceVal, latestSellPriceVal, priceCheckBuyVal, priceCheckSellVal, profitEachVal, potentialProfitVal,
 				roiLabelVal, limitLabelVal).
 				forEach(label -> {
 					label.setHorizontalAlignment(JLabel.RIGHT);
@@ -315,6 +317,21 @@ public class FlippingItemPanel extends JPanel
 						roiLabelVal.setHorizontalAlignment(JLabel.CENTER);
 					}
 				});
+
+
+		latestBuyPriceVal.setForeground(Color.white);
+		latestSellPriceVal.setForeground(Color.white);
+		latestBuyPriceVal.setFont(UIUtilities.runescapeBoldFont);
+		latestSellPriceVal.setFont(UIUtilities.runescapeBoldFont);
+
+		priceCheckBuyVal.setForeground(ColorScheme.GRAND_EXCHANGE_ALCH);
+		priceCheckSellVal.setForeground(ColorScheme.GRAND_EXCHANGE_ALCH);
+
+
+
+		profitEachVal.setForeground(UIUtilities.PROFIT_COLOR);
+		potentialProfitVal.setForeground(UIUtilities.PROFIT_COLOR);
+
 
 		geRefreshLabel.setForeground(ColorScheme.GRAND_EXCHANGE_PRICE);
 		geRefreshLabel.setFont(FontManager.getRunescapeBoldFont());
@@ -367,7 +384,7 @@ public class FlippingItemPanel extends JPanel
 	}
 
 	private void setDescriptionLabels() {
-		Arrays.asList(priceCheckBuyText, priceCheckSellText, latestBuyPriceText, latestSellPriceText, profitEachText, profitTotalText, geLimitText, roiText).
+		Arrays.asList(latestBuyPriceText, latestSellPriceText, priceCheckBuyText, priceCheckSellText, profitEachText, profitTotalText, geLimitText, roiText).
 				forEach(label -> {
 					label.setForeground(ColorScheme.GRAND_EXCHANGE_PRICE);
 					label.setFont(plugin.getFont());
