15626983a09138512778db9fa9a490c31219935d
Zumaad Khan <khanzumaad@gmail.com>
2021-03-19 17:52:37 -0500

    deleted sl4j annotatin and unnecessary imports

--- src/main/java/com/flippingutilities/ui/uiutilities/CustomFonts.java
+++ src/main/java/com/flippingutilities/ui/uiutilities/CustomFonts.java
@@ -1,13 +1,7 @@
 package com.flippingutilities.ui.uiutilities;
-
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.ui.FontManager;
-
-import javax.swing.text.StyleContext;
 import java.awt.*;
-import java.io.IOException;
 
-@Slf4j
 public class CustomFonts {
     public static Font SMALLER_RS_BOLD_FONT = FontManager.getRunescapeBoldFont().deriveFont(14.0F);
 }
