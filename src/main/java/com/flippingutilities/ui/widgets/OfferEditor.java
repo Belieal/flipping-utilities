fc77bf70210aeb1430b5b3d02979c2595e0fafe0
Zumaad Khan <khanzumaad@gmail.com>
2021-04-21 14:44:44 -0500

    refactoring plugin and putting more logic into the individual controller classes

--- src/main/java/com/flippingutilities/ui/widgets/OfferEditor.java
+++ src/main/java/com/flippingutilities/ui/widgets/OfferEditor.java
@@ -37,8 +37,9 @@ import javax.swing.*;
 public class OfferEditor
 {
     private final Client client;
-    private Widget text;
-    private Widget buttonText;
+    private Widget bottomText;
+    private Widget nonWikiText;
+    private Widget wikiText;
 
     public OfferEditor(Widget parent, Client client)
     {
@@ -49,26 +50,25 @@ public class OfferEditor
             return;
         }
 
-        text = parent.createChild(-1, WidgetType.TEXT);
-        buttonText = parent.createChild(-1, WidgetType.TEXT);
+        bottomText = parent.createChild(-1, WidgetType.TEXT);
+        nonWikiText = parent.createChild(-1, WidgetType.TEXT);
+        wikiText = parent.createChild(-1, WidgetType.TEXT);
 
-        prepareTextWidget(buttonText, WidgetPositionMode.ABSOLUTE_TOP, 5);
-        prepareTextWidget(text, WidgetPositionMode.ABSOLUTE_BOTTOM, 15);
-
-        buttonText.setFontId(FontID.QUILL_8);
+        prepareTextWidget(nonWikiText, WidgetTextAlignment.LEFT, WidgetPositionMode.ABSOLUTE_TOP, 5,0);
+        prepareTextWidget(wikiText, WidgetTextAlignment.RIGHT, WidgetPositionMode.ABSOLUTE_TOP, 5, 0);
+        prepareTextWidget(bottomText, WidgetTextAlignment.CENTER, WidgetPositionMode.ABSOLUTE_BOTTOM, 15,0);
+        nonWikiText.setFontId(FontID.QUILL_8);
     }
 
 
-    private void prepareTextWidget(Widget widget, int yMode, int originalY) {
+    private void prepareTextWidget(Widget widget, int xAlignment, int yMode, int yOffset, int xOffset) {
         widget.setTextColor(0x800000);
         widget.setFontId(FontID.VERDANA_11_BOLD);
-        widget.setXPositionMode(WidgetPositionMode.ABSOLUTE_CENTER);
-        widget.setOriginalX(0);
         widget.setYPositionMode(yMode);
-        widget.setOriginalY(originalY);
+        widget.setOriginalX(xOffset);
+        widget.setOriginalY(yOffset);
         widget.setOriginalHeight(20);
-        widget.setXTextAlignment(WidgetTextAlignment.CENTER);
-        widget.setYTextAlignment(WidgetTextAlignment.CENTER);
+        widget.setXTextAlignment(xAlignment);
         widget.setWidthMode(WidgetSizeMode.MINUS);
         widget.setHasListener(true);
         widget.setOnMouseRepeatListener((JavaScriptCallback) ev -> widget.setTextColor(0xFFFFFF));
@@ -81,35 +81,35 @@ public class OfferEditor
         switch (mode)
         {
             case ("quantity"):
-                text.setText("OR click this to use the quantity editor hotkeys!");
-                text.setAction(1, "pic");
-                text.setOnOpListener((JavaScriptCallback) ev -> {
+                bottomText.setText("OR click this to use the quantity editor hotkeys!");
+                bottomText.setAction(1, "pic");
+                bottomText.setOnOpListener((JavaScriptCallback) ev -> {
                     SwingUtilities.invokeLater(()-> {
                         JOptionPane.showMessageDialog(null, Icons.QUANTITY_EDITOR_PIC);
                     });
                 });
 
-                buttonText.setText("click this to set to remaining GE limit: " + value);
-                buttonText.setAction(1, "Set quantity");
-                buttonText.setOnOpListener((JavaScriptCallback) ev ->
+                nonWikiText.setText("click this to set to remaining GE limit: " + value);
+                nonWikiText.setAction(1, "Set quantity");
+                nonWikiText.setOnOpListener((JavaScriptCallback) ev ->
                 {
                     client.getWidget(WidgetInfo.CHATBOX_FULL_INPUT).setText(value + "*");
                     client.setVar(VarClientStr.INPUT_TEXT, String.valueOf(value));
                 });
                 break;
             case ("setSellPrice"):
-                text.setText("OR click this to use the price editor hotkeys!");
-                text.setAction(1, "pic");
-                text.setOnOpListener((JavaScriptCallback) ev -> {
+                bottomText.setText("OR click this to use the price editor hotkeys!");
+                bottomText.setAction(1, "pic");
+                bottomText.setOnOpListener((JavaScriptCallback) ev -> {
                     SwingUtilities.invokeLater(()-> {
                         JOptionPane.showMessageDialog(null, Icons.PRICE_EDITOR_PIC);
                     });
                 });
 
                 if (value != 0) {
-                    buttonText.setText("click this to set to latest margin sell price: " + String.format("%,d", value) + " gp");
-                    buttonText.setAction(1, "Set price");
-                    buttonText.setOnOpListener((JavaScriptCallback) ev ->
+                    nonWikiText.setText("click this to set to latest margin sell price: " + String.format("%,d", value) + " gp");
+                    nonWikiText.setAction(1, "Set price");
+                    nonWikiText.setOnOpListener((JavaScriptCallback) ev ->
                     {
                         client.getWidget(WidgetInfo.CHATBOX_FULL_INPUT).setText(value + "*");
                         client.setVar(VarClientStr.INPUT_TEXT, String.valueOf(value));
@@ -118,17 +118,17 @@ public class OfferEditor
 
                 break;
             case ("setBuyPrice"):
-                text.setText("OR click this to use the price editor hotkeys!");
-                text.setAction(1, "pic");
-                text.setOnOpListener((JavaScriptCallback) ev -> {
+                bottomText.setText("OR click this to use the price editor hotkeys!");
+                bottomText.setAction(1, "pic");
+                bottomText.setOnOpListener((JavaScriptCallback) ev -> {
                     SwingUtilities.invokeLater(()-> {
                         JOptionPane.showMessageDialog(null, Icons.PRICE_EDITOR_PIC);
                     });
                 });
                 if (value != 0) {
-                    buttonText.setText("click this to set to latest margin buy price: " + String.format("%,d", value) + " gp");
-                    buttonText.setAction(1, "Set price");
-                    buttonText.setOnOpListener((JavaScriptCallback) ev ->
+                    nonWikiText.setText("set to insta sell: " + String.format("%,d", value) + " gp");
+                    nonWikiText.setAction(1, "Set price");
+                    nonWikiText.setOnOpListener((JavaScriptCallback) ev ->
                     {
                         client.getWidget(WidgetInfo.CHATBOX_FULL_INPUT).setText(value + "*");
                         client.setVar(VarClientStr.INPUT_TEXT, String.valueOf(value));
@@ -137,7 +137,7 @@ public class OfferEditor
 
                 break;
             case ("reset"):
-                text.setText("");
+                bottomText.setText("");
         }
     }
 }
